30fe0bfea4f2a96d65786d3129655956d4029cb9
SB5032 <sb5032@columbia.edu>
2025-03-07 12:31:16 -0500

    03_07

--- lab2_og/lab2.c
+++ lab2_og/lab2.c
@@ -187,7 +187,7 @@ int main()
             // Removes the last character and updates the display
             message[i--] = ' ';
             update_screen_message();
-            fbputchar('|', (i <=63) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
+            fbputchar('|', (i <=63 && i < 127 ) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
           }
           break;
       
@@ -196,7 +196,7 @@ int main()
             // Moves cursor left if possible and updates the display
             update_screen_message();
             i--;
-            fbputchar('|', (i <=63) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
+            fbputchar('|', (i <=63 && i < 127 ) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
           }
           break;
       
@@ -205,7 +205,7 @@ int main()
             // Moves cursor right if possible and updates the display
             i++;
             update_screen_message();
-            fbputchar('|', (i <=63) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
+            fbputchar('|', (i <=63 && i < 127 ) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
           }
           break;
       
@@ -214,7 +214,7 @@ int main()
             // Moves cursor up one line if possible and updates the display
             i -= 64;
             update_screen_message();
-            fbputchar('|', (i <=63) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
+            fbputchar('|', (i <=63 && i < 127 ) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
           }
           break;
       
@@ -223,7 +223,7 @@ int main()
             // Moves cursor down one line if possible and updates the display
             i += 64;
             update_screen_message();
-            fbputchar('|', (i <=63) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
+            fbputchar('|', (i <=63 && i < 127 ) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
           }
           break;
       
@@ -232,7 +232,7 @@ int main()
         //     // Handles printable characters by adding them to the message buffer
         //     message[++i] = curr_char;
         //     update_screen_message();
-        //     fbputchar('|', (i <=63) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
+        //     fbputchar('|', (i <=63 && i < 127 ) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
         //   }
         //   break;
 		default:
@@ -249,7 +249,7 @@ int main()
 
     // Update the display
     update_screen_message();
-    fbputchar('|', (i <=63) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
+    fbputchar('|', (i <=63 && i < 127 ) ? 21 : 22, (i + 1) % 64);  // Blinking cursor
   }
   break;
 
