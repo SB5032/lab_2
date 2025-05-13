467be5558d32a6912b8c6a4fdfdf3aff72afcecc
SB5032 <sb5032@columbia.edu>
2025-05-13 16:41:12 -0400

    texts

--- new/game/demo.c
+++ new/game/demo.c
@@ -343,9 +343,9 @@ int main(void) {
     if (game_level_main >= 3) { fill_nightsky_and_grass(); } else { fill_sky_and_grass(); }
     //vga_present_frame(); present_sprites();   
     write_text("scream", 6, 13, 16); write_text("jump", 4, 13, 22);
-    write_text("press", 5, 19, 9); write_text("x", 1, 19, 15); 
-    write_text("key", 3, 19, 17); write_text("to", 2, 19, 21); 
-    write_text("start", 5, 19, 24);
+    write_text("press", 5, 16, 12); write_text("x", 1, 16, 18); 
+    write_text("key", 3, 16, 20); write_text("to", 2, 16, 24); 
+    write_text("start", 5, 16, 27);
 
 
     vga_present_frame(); present_sprites();
