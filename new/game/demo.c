13453f5d9a0fbec94a215c53908026b0b5af1c1a
Ananya247 <am6542@micro01.ee.columbia.edu>
2025-05-12 20:09:28 -0400

    nope

--- new/game/demo.c
+++ new/game/demo.c
@@ -318,7 +318,7 @@ int main(void) {
     cleartiles(); 
     clearSprites_buffered(); 
     fill_sky_and_grass(); 
-    // vga_present_frame(); 
+    vga_present_frame(); 
     // present_sprites();   
     write_text((unsigned char *)"scream", 6, 13, 13); write_text((unsigned char *)"jump", 4, 13, 20);
     write_text((unsigned char *)"press", 5, 19, 8); write_text((unsigned char *)"x", 1, 19, 14); 
