22819917f9a8eb8843ba9664d83f8ca59ebd7514
Ananya247 <am6542@micro01.ee.columbia.edu>
2025-05-12 20:07:22 -0400

    intial fix

--- new/game/demo.c
+++ new/game/demo.c
@@ -315,12 +315,17 @@ int main(void) {
     }
 
 
-    cleartiles(); clearSprites_buffered(); fill_sky_and_grass(); vga_present_frame(); present_sprites();   
+    cleartiles(); 
+    clearSprites_buffered(); 
+    fill_sky_and_grass(); 
+    // vga_present_frame(); 
+    // present_sprites();   
     write_text((unsigned char *)"scream", 6, 13, 13); write_text((unsigned char *)"jump", 4, 13, 20);
     write_text((unsigned char *)"press", 5, 19, 8); write_text((unsigned char *)"x", 1, 19, 14); 
     write_text((unsigned char *)"key", 3, 19, 20); write_text((unsigned char *)"to", 2, 19, 26); 
     write_text((unsigned char *)"start", 5, 19, 29);
     vga_present_frame(); 
+    present_sprites();  
 
 	game_restart_point: ;
 	// pthread_t controller_thread;
